cc9ccac2d7e7a9b4f48146d6cb54f46b6f83bd03
solarpanel0910 <sola12144@gmail.com>
2026-01-26 12:13:29 +0900

    データ、グラフの調整

--- app.py
+++ app.py
@@ -15,16 +15,28 @@ df = df.rename(columns={
     "曜日" : "day",
 })
 
+st.write(df.columns)
+
 df['sleep'] = pd.to_numeric(df['sleep'], errors='coerce')
 
 df = df[["sleep", "age", "sex", "day"]]
 
+
 with st.sidebar: 
+    sex = st.selectbox("あなたの性別を入力してください",
+                       ['男性', '女性'])
+    
     age = st.number_input("あなたの年齢を入力してください",
                           min_value=15,
                           max_value=120,
                           value=40,
                           step=1)
+    
+    sleep_hour = st.number_input("あなたの睡眠時間を入力してください(1時間単位)",
+                                 min_value=0.0,
+                                 max_value=24.0,
+                                 value=8.0,
+                                 step=0.5)
     
     theme = st.selectbox("テーマを選択してください",
                      ["年齢", "性別", "曜日",])
@@ -45,4 +57,7 @@ fig = px.bar(df.reset_index(),
              title=f"{theme}ごとの睡眠時間比較")
 st.plotly_chart(fig)
     
-
+# colu = st.columns(3)
+# colu[1].metric("Sleep Hour")
+# colu[2].metric
+# colu[3].metric
